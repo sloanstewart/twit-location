f6edb5e5ba0092ca35434d892250c0580814f007
Sloan <sloanstewartdev@gmail.com>
2017-09-27 02:54:34 +0000

    query

--- index.js
+++ index.js
@@ -291,7 +291,7 @@ function log(){
 
 function getDataFromApi(lat, lng, rad, callback) {
   var query = {
-    q: 'trap', // query is required
+    q: '', // query is required
     // geocode: lat+','+lng+','+rad+'km',
     geocode: '33.7800000,-84.2700000,1km',
     result_type: 'recent',
